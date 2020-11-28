eaf79c03e1753b05d432a386c3acc7b7c687ee70
Santhosh Jackson <santhosh.jack.j@gmail.com>
2020-11-28 22:24:01 +0000

    Added Swagger for api doc and testing

---
+++ src/main/java/com/payment/credit/config/PaymentsSwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.payment.credit.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author Santhosh Jackson
+ **/
+@Configuration
+@EnableSwagger2
+public class PaymentsSwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.payment.credit.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+}
